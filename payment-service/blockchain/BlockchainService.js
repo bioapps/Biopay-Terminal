a2cd0b104a156adc07244b8eb07dae5fa325ca3b
Hektor Wallin <hektorw@gmail.com>
2016-02-12 14:13:31 +0100

    Using local wallet service method for payment

--- payment-service/blockchain/BlockchainService.js
+++ payment-service/blockchain/BlockchainService.js
@@ -91,76 +91,14 @@ module.exports = class BlockchainService {
 	// Unencrypted payment
 	// Goes directly to payment service, credentials are cleartext
 	//
-	makePayment(paymentData, amount/*, currency*/) {
-		const amountInSatoshi = BitcoinUtils.btcToSatoshi(amount);
-
-		const identifier = paymentData.username;
-		const password = paymentData.password;
-
-		const wallet = new blockchain.MyWallet(identifier, password, {
-			apiCode: this.config.walletApiCode,
-			apiHost: this.config.apiV2HostUrl
-		});
-
-		this.fixMyWalletParamsHack(wallet);
-
-		return Promise.all([
-				this.receiver.generate(),	// eslint-disable-line indent
-				wallet.login()				// eslint-disable-line indent
-			])								// eslint-disable-line indent
-			.then(values => {
-				const generatedAddress = values[0].address;
-				
-				console.log(`Making a new payment (BTC ${amount}) to (Address ${generatedAddress})`);
-				return wallet.send(generatedAddress, amountInSatoshi);
-			})
-			.then(paymentResponse => {
-				//
-				// Should log out wallet
-				//
-				return paymentResponse;
-			})
-			.catch(error => {
-				if (typeof error === 'string') {
-					try {
-						error = JSON.parse(error);
-					} catch (ex) {}	// eslint-disable-line no-empty
-				}
-
-				throw {
-					name: 'blockchain-payment',
-					message: error
-				};
-			});
-	}
-
-	//
-	// Last version checked where this fix is needed: "blockchain.info": "2.2.0"
-	// 
-	// MyWallet implementation sets the query parameter name for receiving addres as "address" but the API expects the query parameter name "to".
-	// The MyWallet has a function called "getParams" which returns an object that the query parameters are added to.
-	// We override this function and add a getter with the name "to" to the returned object that returns the value of "address".
-	//
-	fixMyWalletParamsHack(wallet) {
-		const _getParams = wallet.getParams.bind(wallet);
-
-		wallet.getParams = () => {
-			const params = _getParams();
-
-			Object.defineProperty(params, 'to', {
-				enumerable: true,
-				configurable: true,
-				get() {
-					return this.address;
-				},
-				set(value) {
-					// If the "to" value is ever set, we remove our implementation and make it a regular property.
-					delete this.to;
-					this.to = value;
-				}
-			});
-
-			return params;
+	makePayment(paymentData, amountInBtc/*, currency*/) {
+		const amountInSatoshi = BitcoinUtils.btcToSatoshi(amountInBtc);
+		const credentials = {
+			identifier: paymentData.username,
+			password: paymentData.password
 		};
+
+		return this.receiver.generate()
+			.then(response => this.walletService.makePayment(credentials, response.address, amountInSatoshi));
 	}
 };
